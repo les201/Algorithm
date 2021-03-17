4809b9912a24f9655f2cf8d64fbedc87650bf005
LEE <LEE@DESKTOP-7TN5DVA>
2021-03-17 18:34:40 +0900

    1000 A+B

---
+++ src/B1000.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class B1000 {
+
+	public static void main(String[] args) {
+		int a,b;
+		Scanner sc = new Scanner (System.in);
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		System.out.println(a+b);
+
+	}
+
+}
