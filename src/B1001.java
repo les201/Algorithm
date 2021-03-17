d97e154adee730fae65ba10ef29c843bc5a2fbb4
LEE <LEE@DESKTOP-7TN5DVA>
2021-03-17 18:40:17 +0900

    This is Commit Message

---
+++ src/B1001.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class B1001 {
+	public static void main (String[] args) {
+		
+		int a,b;
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		System.out.print(a-b);
+		
+	}
+}
