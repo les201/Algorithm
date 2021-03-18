ba6aaa52d2d9fc0cd3e6a6e28d028028a2f989fa
LEE <LEE@DESKTOP-7TN5DVA>
2021-03-18 23:51:26 +0900

    This is Commit Message!!

---
+++ src/B10869.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class B10869 {
+	public static void main (String[] args) {
+		
+		int a,b;
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
+}
