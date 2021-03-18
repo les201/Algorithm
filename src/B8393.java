e202497da0009e270e186bb3bcc90b6eb74bd010
LEE <LEE@DESKTOP-7TN5DVA>
2021-03-18 23:45:19 +0900

    This is Commit Message

---
+++ src/B8393.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class B8393 {
+	public static void main (String[] args) {
+		int a,b = 0;
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt();
+		
+		for(int i = 0 ; i <= a ;i++) {
+			b=b+i;
+		}
+		System.out.println(b);		
+		}
+}
